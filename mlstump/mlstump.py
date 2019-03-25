2b201cb74605dfc0113be143df72a1e3d1daa322
Neal Fultz <nfultz@gmail.com>
2019-03-25 03:24:44 +0000

    WIP

--- mlstump/mlstump.py
+++ mlstump/mlstump.py
@@ -1,3 +1,41 @@
 # -*- coding: utf-8 -*-
 
+from fire import Fire
+
+from functools import partial
+
 """Main module."""
+
+
+def provides(*args):
+    def impl(f):
+        f.provides = args
+        return f
+    return impl
+
+def get_needs(needs, f, self, *args, **kwargs):
+    for need in needs:
+        if not hasattr(self, need):
+            print("needs", need)
+    f(self, *args)
+    return self
+
+
+def needs(*args):
+    def wrap(f):
+        return partial(get_needs, args, f)
+    return wrap
+
+
+class A:
+    @provides("d")
+    def a(self):
+        self.d = True
+        return self
+
+    @needs("d")
+    def b(self):
+        print(self.d)
+
+
+
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = [ 'fire' ]
 
 setup_requirements = ['pytest-runner', ]
 
@@ -25,11 +25,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
